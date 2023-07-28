c92e9a709e2ccbd27efe4d88aeddede55f5bee7a
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-07-28 13:44:39 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,13 +2,14 @@ function capitalize() {
     // Get the input text from the textarea
     const inputText = document.getElementById("input-text").value;
 
+    // Create a new FormData object and add the paragraph field
+    const formData = new FormData();
+    formData.append("paragraph", inputText);
+
     // Make an asynchronous POST request to the Flask backend
     fetch("/", {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ paragraph: inputText }),
+        body: formData,
     })
     .then(response => response.json())
     .then(data => {
