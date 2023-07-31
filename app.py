df24405d7e54f36c0ed5d920cbf9af527a43a61f
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-07-31 23:18:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,26 @@
 from flask import Flask, request, jsonify, render_template
 from transformers import pipeline
-from celery import Celery
 
 app = Flask(__name__)
 
 # Initialize the summarization pipeline with the desired model and tokenizer
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn", tokenizer="facebook/bart-large-cnn")
 
-# Configure Celery
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'  # Replace with your Redis URL
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'  # Replace with your Redis URL
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
-
-# Asynchronous Celery task for summarization
-@celery.task
-def summarize_text(text, max_length=300, min_length=50):
-    return summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)[0]['summary_text']
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         paragraph = request.form.get("paragraph")
-        # Start the asynchronous Celery task for summarization
-        summarized_text = summarize_text.delay(paragraph).get()
+        # Use the summarization pipeline to generate the summary
+        summarized_text = generate_summary(paragraph, max_length=300, min_length=50)
         return jsonify({"summarized_text": summarized_text})
 
     return render_template("index.html")
+
+def generate_summary(text, max_length=300, min_length=50):
+    # Generate the summary using the summarization pipeline
+    # You can adjust max_length and min_length as per your requirements
+    summary = summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)[0]['summary_text']
+    return summary
 
 if __name__ == "__main__":
     app.run(debug=True)
