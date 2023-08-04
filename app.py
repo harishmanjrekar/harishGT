1ae6da6392fa9e3cc613dfd7b2ccfc3880e5e479
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-08-04 21:43:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask, request, jsonify, render_template
-from summa import summarizer
+from transformers import pipeline
 
 app = Flask(__name__)
+
+# Initialize the summarization pipeline with the Pegasus model
+summarizer = pipeline("summarization", model="google/pegasus-xsum")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         paragraph = request.form.get("paragraph")
-        summarized_text = generate_summary(paragraph)  # Using TextRank for summarization
+        # Use the summarization pipeline to generate the summary
+        summarized_text = generate_summary(paragraph)
         return jsonify({"summarized_text": summarized_text})
 
     return render_template("index.html")
 
-def generate_summary(text, ratio=0.2):
-    # Generate the summary using the TextRank algorithm
-    summary = summarizer.summarize(text, ratio=ratio)
+def generate_summary(text):
+    # Generate the summary using the summarization pipeline (Pegasus)
+    summary = summarizer(text, max_length=150, min_length=50, do_sample=True)[0]["summary_text"]
     return summary
 
 if __name__ == "__main__":
