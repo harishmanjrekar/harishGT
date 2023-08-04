d0024fcade15f969ab13c4f76c748b5d558af63b
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-08-04 13:52:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
 
     return render_template("index.html")
 
-def generate_summary(text, ratio=0.3):
+def generate_summary(text, ratio=0.2):
     # Generate the summary using the TextRank algorithm
     summary = summarizer.summarize(text, ratio=ratio)
     return summary
