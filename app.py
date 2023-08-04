3f39f793ee22bfca0d44943a5f85a792d655dca8
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-08-04 21:12:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         paragraph = request.form.get("paragraph")
-        summarized_text = generate_summary(paragraph)   Using TextRank for summarization
+        summarized_text = generate_summary(paragraph)  # Using TextRank for summarization
         return jsonify({"summarized_text": summarized_text})
 
     return render_template("index.html")
 
 def generate_summary(text, ratio=0.2):
     # Generate the summary using the TextRank algorithm
-        summary = summarizer.summarize(text, ratio=ratio)
-        return summary
+    summary = summarizer.summarize(text, ratio=ratio)
+    return summary
 
 if __name__ == "__main__":
     app.run(debug=True)
