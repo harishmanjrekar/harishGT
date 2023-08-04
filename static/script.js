c0d405585ec97886c2599be5a7217e03e41934c1
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-08-04 13:52:25 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,47 +1,26 @@
 function summarize() {
+    // Get the input text from the textarea
     const inputText = document.getElementById("input-text").value;
+
+    // Create a new FormData object and add the paragraph field
     const formData = new FormData();
     formData.append("paragraph", inputText);
 
+    // Make an asynchronous POST request to the Flask backend
     fetch("/", {
         method: "POST",
         body: formData,
     })
     .then(response => response.json())
     .then(data => {
-        const chatBox = document.getElementById("chat-box");
-        chatBox.innerHTML += `<div class="message user">You: ${inputText}</div>`;
-        chatBox.innerHTML += `<div class="message bot">Bot: ${data.summarized_text}</div>`;
+        // Display the summarized text in the output box
+        const outputBox = document.getElementById("output-box");
+        outputBox.innerHTML = data.summarized_text;
 
-        chatBox.scrollTop = chatBox.scrollHeight;
+        // Clear the input textarea
         document.getElementById("input-text").value = "";
-        
-        // Display the summarized text in the output section
-        const outputText = document.getElementById("output-text");
-        outputText.textContent = data.summarized_text;
     })
     .catch(error => {
         console.error("Error:", error);
     });
 }
-
-function clearAll() {
-    const chatBox = document.getElementById("chat-box");
-    chatBox.innerHTML = '<div class="message bot">Enter a paragraph to summarize:</div>';
-    
-    const inputText = document.getElementById("input-text");
-    inputText.value = "";
-
-    const outputText = document.getElementById("output-text");
-    outputText.textContent = "";
-}
-
-// Function to make the container full screen
-function makeFullscreen() {
-    const container = document.getElementById("container");
-    container.style.width = window.innerWidth + "px";
-    container.style.height = window.innerHeight + "px";
-}
-
-// Call the function to make the container full screen when the page loads
-window.onload = makeFullscreen;
