7df301e9b2a0295ab4818932a4c8a3ad8329da16
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-08-04 17:57:55 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -16,6 +16,9 @@ function summarize() {
         // Display the summarized text in the output box
         const outputBox = document.getElementById("output-box");
         outputBox.innerText = data.summarized_text;
+
+        // Update word count for input and output sections
+        countWords();
     })
     .catch(error => {
         console.error("Error:", error);
@@ -26,4 +29,19 @@ function clearText() {
     // Clear the input and output textareas
     document.getElementById("input-text").value = "";
     document.getElementById("output-box").innerText = "";
+
+    // Update word count to 0 after clearing text
+    countWords();
+}
+
+function countWords() {
+    const inputText = document.getElementById("input-text").value;
+    const inputWordCount = inputText.trim().split(/\s+/).length;
+    const inputWordCountElement = document.getElementById("input-word-count");
+    inputWordCountElement.textContent = `Word count: ${inputWordCount}`;
+
+    const outputText = document.getElementById("output-box").textContent;
+    const outputWordCount = outputText.trim().split(/\s+/).length;
+    const outputWordCountElement = document.getElementById("output-word-count");
+    outputWordCountElement.textContent = `Word count: ${outputWordCount}`;
 }
