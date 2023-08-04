5f4518d4f67e0cf015868ce21a1ab227b2d222f2
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-08-04 17:19:30 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,6 +1,12 @@
+function updateWordCount(type) {
+    const text = document.getElementById(`${type}-box`).innerText;
+    const words = text.trim().split(/\s+/).length;
+    document.getElementById(`${type}-word-count`).innerText = words;
+}
+
 function summarize() {
-    // Get the input text from the textarea
-    const inputText = document.getElementById("input-text").value;
+    // Get the input text from the input-box
+    const inputText = document.getElementById("input-box").innerText;
 
     // Create a new FormData object and add the paragraph field
     const formData = new FormData();
@@ -16,14 +22,17 @@ function summarize() {
         // Display the summarized text in the output box
         const outputBox = document.getElementById("output-box");
         outputBox.innerText = data.summarized_text;
+        updateWordCount('output');
     })
     .catch(error => {
         console.error("Error:", error);
     });
 }
 
-function clearText() {
-    // Clear the input and output textareas
-    document.getElementById("input-text").value = "";
+function clearInputOutput() {
+    // Clear the input and output boxes and word counters
+    document.getElementById("input-box").innerText = "Enter a paragraph to summarize:";
     document.getElementById("output-box").innerText = "";
+    updateWordCount('input');
+    updateWordCount('output');
 }
