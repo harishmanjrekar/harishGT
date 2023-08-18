c904ecede59e755e08dbc3a4c7e44dbe0e449d7b
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-08-18 16:56:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,14 +16,7 @@ def generate_summary(text, ratio=0.2):
     # Generate the summary using the TextRank algorithm
     summary = summarize(
         text,
-        ratio=ratio,
-        word_count=None,  # Specify a word count limit here
-        split=False,  # Set to True if you want sentence splitting
-        remove_redundancy=True,
-        diversity=0.5,  # Set a diversity parameter for sentence selection
-        normalization=None,  # Specify a normalization function
-        similarity="jaccard",  # Use "cosine" or "jaccard" for similarity
-        min_length=5  # Specify a minimum sentence length
+        ratio=ratio
     )
     return summary
 
